be43df9ef3afcc2fb4dea110ac8d7c3297da481d
Bernhard Egger <bernhard@csap.snu.ac.kr>
2016-07-28 05:36:48 +0900

    Step 6 (partial; change mock data provider to http in-memory db but no add/edit/delete) of tutorial (https://angular.io/docs/ts/latest/tutorial/toh-pt6.html)

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,18 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+//import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
+    private heroesUrl = 'app/heroes';
+
+    constructor(private http: Http) { }
+
     getHeroes() {
-        return Promise.resolve(HEROES);
+        return this.http.get(this.heroesUrl)
+            .toPromise()
+            .then(response => response.json().data as Hero[])
+            .catch(this.handleError);
     }
 
     getHeroesSlowly() {
-        return new Promise<Hero[]>(resolve =>
-            setTimeout(() => resolve(HEROES), 2975)
-        );
+        //return setTimeout(function () { this.getHeroes(); }, 1000);
+        return this.getHeroes();
     }
     
     getHero(id: number) {
@@ -20,4 +28,8 @@ export class HeroService {
             .then(heroes => heroes.find(hero => hero.id === id));
     }
 
+    private handleError(error: any) {
+        console.error('Cannot get data!', error);
+        return Promise.reject(error.message || error);
+    }
 }
